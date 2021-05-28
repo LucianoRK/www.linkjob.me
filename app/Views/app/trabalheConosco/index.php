9da4e84ca33ff5072cf2e4eacb8492676997f305
Fernando Stavski <fernando.stavski@gmail.com>
2021-05-27 23:15:33 -0300

    Formulário trabalhe conosco

--- app/Views/app/trabalheConosco/index.php
+++ app/Views/app/trabalheConosco/index.php
@@ -1,8 +1,9 @@
+<div id="page-home"></div>
 <div class="lower-page element-color-switch" id="page-about">
     <div class="center-container">
         <div class="center-block">
             <div class="container sections">
-                <div class="inner-divider"></div>
+            <div class="inner-divider"></div>
                 <div class="inner-divider"></div>
                 <div class="row">
                     <div class="col-md-12">
@@ -20,7 +21,25 @@
                             </div> 
                             <div class="form-group row">
                                 <div class="col-md-2"></div>
+                                <label for="genero" class="col-md-2 col-form-label"><span class="text-danger">*</span>Gênero:</label>
+                                <div class="col-md-6">
+                                    <select name="genero" id="genero" value="" class="form-control">
+                                        <option selected value=""> Selecione uma opção </option>
+                                        <option value="Masculino"> Masculino </option>
+                                        <option value="Femenino"> Femenino </option>
+                                    </select>
+                                </div>
+                            </div> 
+                            <div class="form-group row">
+                                <div class="col-md-2"></div>
                                 <label for="nome" class="col-md-2 col-form-label"><span class="text-danger">*</span>Data de Nascimento:</label>
+                                <div class="col-md-6">
+                                    <input name="nome" value="" type="text" id="nome" class="form-control">
+                                </div>
+                            </div> 
+                            <div class="form-group row">
+                                <div class="col-md-2"></div>
+                                <label for="nome" class="col-md-2 col-form-label"><span class="text-danger">*</span>CPF:</label>
                                 <div class="col-md-6">
                                     <input name="nome" value="" type="text" id="nome" class="form-control">
                                 </div>
@@ -34,25 +53,57 @@
                             </div> 
                             <div class="form-group row">
                                 <div class="col-md-2"></div>
-                                <label for="genero" class="col-md-2 col-form-label"><span class="text-danger">*</span>Gênero:</label>
+                                <label for="nome" class="col-md-2 col-form-label">CNPJ MEI:</label>
+                                <div class="col-md-6">
+                                    <input name="nome" value="" type="text" id="nome" class="form-control">
+                                </div>
+                            </div> 
+                            <hr>
+                            <div class="form-group row">
+                                <div class="col-md-2"></div>
+                                <label for="genero" class="col-md-2 col-form-label"><span class="text-danger">*</span>Escolaridade:</label>
                                 <div class="col-md-6">
                                     <select name="genero" id="genero" value="" class="form-control">
                                         <option selected value=""> Selecione uma opção </option>
-                                        <option value="Masculino"> Masculino </option>
-                                        <option value="Femenino"> Femenino </option>
+                                        <option value="Fundamental"> Fundamental </option>
+                                        <option value="Medio"> Ensino Médio </option>
+                                        <option value="Superior"> Graduação </option>
+                                        <option value="Superior"> Pós Graduação </option>
                                     </select>
                                 </div>
                             </div> 
                             <div class="form-group row">
                                 <div class="col-md-2"></div>
-                                <label for="nome" class="col-md-2 col-form-label"><span class="text-danger">*</span>Endereço:</label>
+                                <label for="nome" class="col-md-2 col-form-label">Área:</label>
                                 <div class="col-md-6">
                                     <input name="nome" value="" type="text" id="nome" class="form-control">
                                 </div>
                             </div> 
                             <div class="form-group row">
                                 <div class="col-md-2"></div>
-                                <label for="nome" class="col-md-2 col-form-label"><span class="text-danger">*</span>CEP:</label>
+                                <label for="nome" class="col-md-2 col-form-label">Instituição:</label>
+                                <div class="col-md-6">
+                                    <input name="nome" value="" type="text" id="nome" class="form-control">
+                                </div>
+                            </div> 
+                            <div class="form-group row">
+                                <div class="col-md-2"></div>
+                                <label for="nome" class="col-md-2 col-form-label">Idiomas:</label>
+                                <div class="col-md-6">
+                                    <input name="nome" value="" type="text" id="nome" class="form-control">
+                                </div>
+                            </div> 
+                            <hr>
+                            <div class="form-group row">
+                                <div class="col-md-2"></div>
+                                <label for="nome" class="col-md-2 col-form-label"><span class="text-danger">*</span>Endereço:</label>
+                                <div class="col-md-6">
+                                    <input name="nome" value="" type="text" id="nome" class="form-control" placeholder="Endereço completo">
+                                </div>
+                            </div> 
+                            <div class="form-group row">
+                                <div class="col-md-2"></div>
+                                <label for="nome" class="col-md-2 col-form-label">Complemento:</label>
                                 <div class="col-md-6">
                                     <input name="nome" value="" type="text" id="nome" class="form-control">
                                 </div>
@@ -77,14 +128,84 @@
                             </div> 
                             <div class="form-group row">
                                 <div class="col-md-2"></div>
+                                <label for="nome" class="col-md-2 col-form-label"><span class="text-danger">*</span>CEP:</label>
+                                <div class="col-md-6">
+                                    <input name="nome" value="" type="text" id="nome" class="form-control">
+                                </div>
+                            </div> 
+                            <hr>
+                            <div class="form-group row">
+                                <div class="col-md-2"></div>
                                 <label for="nome" class="col-md-2 col-form-label"><span class="text-danger">*</span>Celular:</label>
                                 <div class="col-md-6">
                                     <input name="nome" value="" type="text" id="nome" class="form-control">
                                 </div>
                             </div> 
-                            
-                           
-                            
+                            <div class="form-group row">
+                                <div class="col-md-2"></div>
+                                <label for="nome" class="col-md-2 col-form-label"><span class="text-danger">*</span>Instagram:</label>
+                                <div class="col-md-6">
+                                    <input name="nome" value="" type="text" id="nome" class="form-control" placeholder="Link do seu Instagram">
+                                </div>
+                            </div> 
+                            <hr>
+                            <div class="form-group row">
+                                <div class="col-md-2"></div>
+                                <label for="nome" class="col-md-2 col-form-label"><span class="text-danger">*</span>Banco:</label>
+                                <div class="col-md-6">
+                                    <input name="nome" value="" type="text" id="nome" class="form-control">
+                                </div>
+                            </div> 
+                            <div class="form-group row">
+                                <div class="col-md-2"></div>
+                                <label for="nome" class="col-md-2 col-form-label"><span class="text-danger">*</span>Agência:</label>
+                                <div class="col-md-6">
+                                    <input name="nome" value="" type="text" id="nome" class="form-control">
+                                </div>
+                            </div> 
+                            <div class="form-group row">
+                                <div class="col-md-2"></div>
+                                <label for="nome" class="col-md-2 col-form-label"><span class="text-danger">*</span>Conta:</label>
+                                <div class="col-md-6">
+                                    <input name="nome" value="" type="text" id="nome" class="form-control">
+                                </div>
+                            </div> 
+                            <div class="form-group row">
+                                <div class="col-md-2"></div>
+                                <label for="nome" class="col-md-2 col-form-label">Pix:</label>
+                                <div class="col-md-6">
+                                    <input name="nome" value="" type="text" id="nome" class="form-control">
+                                </div>
+                            </div> 
+                            <div class="form-group row">
+                                <div class="col-md-2"></div>
+                                <label for="nome" class="col-md-2 col-form-label">PicPay:</label>
+                                <div class="col-md-6">
+                                    <input name="nome" value="" type="text" id="nome" class="form-control">
+                                </div>
+                            </div> 
+                            <hr>
+                            <div class="form-group row">
+                                <div class="col-md-2"></div>
+                                <label for="nome" class="col-md-2 col-form-label">Email:</label>
+                                <div class="col-md-6">
+                                    <input name="nome" value="" type="text" id="nome" class="form-control">
+                                </div>
+                            </div> 
+                            <div class="form-group row">
+                                <div class="col-md-2"></div>
+                                <label for="nome" class="col-md-2 col-form-label">Senha:</label>
+                                <div class="col-md-6">
+                                    <input name="nome" value="" type="password" id="nome" class="form-control">
+                                </div>
+                            </div> 
+                            <div class="form-group row">
+                                <div class="col-md-2"></div>
+                                <label for="nome" class="col-md-2 col-form-label">Repita Senha:</label>
+                                <div class="col-md-6">
+                                    <input name="nome" value="" type="password" id="nome" class="form-control">
+                                </div>
+                            </div> 
                         </form>
                     </div>
                 </div>
