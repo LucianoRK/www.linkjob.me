273352621174948489adfb42d534f21f2cc12e76
Fernando Stavski <fernando.stavski@gmail.com>
2021-06-06 12:22:10 -0300

    Correção

--- app/Views/app/perfil/pendentesAprovacao.php
+++ app/Views/app/perfil/pendentesAprovacao.php
@@ -10,7 +10,6 @@
                         <h2 class="section-heading-all">
                             <span></span>Perfis Pendentes de Aprovação
                         </h2>
-                        <div class="inner-divider"></div>
                         <div class="row">
                             <div class="table-responsive">
                                 <table id="table-aprovacao" class="table table-bordered dataTable dtr-inline">
@@ -23,22 +22,22 @@
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <?php if ($perfis) { ?>
+                                        <?php if (!empty($perfis)) { ?>
                                             <?php foreach ($perfis as $perfil) { ?>
                                                 <tr class="tr-perfil">
                                                     <td>
-                                                        <?php echo $perfil->usuario_id; ?>
+                                                        <?php echo $perfil['usuario_id']; ?>
                                                     </td>
                                                     <td>
-                                                        <?php echo $perfil->perfil_id; ?>
+                                                        <?php echo $perfil['perfil_id']; ?>
                                                     </td>
                                                     <td>
-                                                        <?php echo $perfil->created_at; ?>
+                                                        <?php echo $perfil['created_at']; ?>
                                                     </td>
                                                     <td width="130">
-                                                        <button type="button" class="btn btn-default btn-aprovar-perfil" title="Aprovar Perfil"><i class="text-success fas fa-check"></i></button>
-                                                        <button type="button" class="btn btn-default btn-recusar-perfil" title="Recusar Perfil"><i class="text-danger fas fa-trash"></i></button>
-                                                        <a href="ver-erfil" target="_blank" type="button" class="btn btn-default" title="Ver perfil"><i class="fas fa-eye"></i></a>
+                                                        <button type="button" class="btn btn-sm btn-default btn-aprovar-perfil" title="Aprovar Perfil"><i class="text-success fas fa-check"></i></button>
+                                                        <button type="button" class="btn btn-sm btn-default btn-recusar-perfil" title="Recusar Perfil"><i class="text-danger fas fa-trash"></i></button>
+                                                        <button type="button" class="btn btn-sm btn-default btn-ver-perfil" title="Ver perfil"><i class="fas fa-eye"></i></a>
                                                     </td>
                                                 </tr>
                                             <?php } ?>
